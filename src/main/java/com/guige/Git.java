ff2c6c15053bb8e6567f8e15cf6195bee1cc6da3
guijingsheng <2014807786@qq.com>
2022-07-26 14:58:48 +0800

    hot第1次修改

--- src/main/java/com/guige/Git.java
+++ src/main/java/com/guige/Git.java
@@ -4,5 +4,7 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git11");
+        System.out.println("hello git22");
+
     }
 }
